41819492dc2babeb4459fb7344f339fd03dd7536
omhsyed <omhsyed@gmail.com>
2026-03-14 15:17:00 -0500

    Created randomly selected teams as test vector, redefined off and def stats in simulate_game function to be simpler/more readable and work correctly with each column stat, converted stats in original df to all be numeric.

--- Simulation Script.R
+++ Simulation Script.R
@@ -30,8 +30,9 @@ sesh = read_html_live(bracket_url)
 seed_tags = html_elements(sesh, "span.overline color_lvl_-5")
 team_tags = html_elements(sesh, "body body_2 color_lvl_-5")
 
-seed_nums = html_text(seed_tags)
-team_names = html_text(team_tags)
+seeds = html_text(seed_tags)
+teams = html_text(team_tags)
+
 
 
 # kenpom alternative (barttorvik) stats page
@@ -48,7 +49,13 @@ colnames(df) <- df[1,]
 
 df <- df[-1,]
 
-#print(df)
+df[, 6:24] <- lapply(df[, 6:24], as.numeric)
+
+#df[, 2] <- lapply(df[,2], substring(df[,2], 1, grep(" ", df[,2])))
+#df[[2]] <- sub("   vs\\..*", "", df[[2]])
+#substring(str, 1, gregexpr("v", str)[[1]][1] - 4)
+
+print(df)
 
 
 
@@ -56,30 +63,85 @@ df <- df[-1,]
 
 
 
-k = 0.5
-w = c(0.4, 0.25, 0.2, 0.15)
+# test values for seed and teams until bracket is finalized
+rand64 <- sample(1:120, 64)
+
+seeds <- numeric(0)
+teams <- character(0)
+
+for (i in 1:length(rand64)) 
+{
+  seeds <- c(seeds, as.numeric(df[df$Rk == rand64[i], 1][[1]]))
+}
+for (i in 1:length(rand64)) 
+{
+  teams <- c(teams, df[df$Rk == rand64[i], 2][[1]])
+}
+
+
+team_info = tibble(Name = teams, Seed = seeds)
+print(team_info)
+team_vect <- team_info[,1][[1]]
+print(team_vect)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+k = 0.1
+w = c(0.5, 0.2, 0.15, 0.1, 0.05)
 
 Px <- function(ox, dx, oy, dy) 
 {
-  i = 1:4
+  i = 1:5
   return((1 + exp(-k * (sum(w[i]*((ox[i] - dy[i]) - (oy[i] - dx[i]))))))^(-1))
 }
 
 
-Px_new(c(55, 13, 39, 32), c(47, 11, 26, 19), c(57, 13, 35, 28), c(52, 15, 26, 27))
+Px(c(-5, 55, 13, 39, 32), c(0, 47, 11, 26, 19), c(-7, 57, 13, 35, 28), c(0, 52, 15, 26, 27))
 
 
 
-team_vect <- as.character(1:64)
 
 
 simulate_game <- function(teamX, teamY)
 {
-  x_off_stats = as.numeric(unlist(df[df$Rk == teamX, seq(9, 15, 2)], use.names = FALSE))
-  x_def_stats = as.numeric(unlist(df[df$Rk == teamX, seq(10, 16, 2)], use.names = FALSE))
+
+  x_seed = team_info[team_info$Name == teamX, 2][[1]]
+  x_off_stats = c(-1*x_seed)
+  x_def_stats = c(0)
   
-  y_off_stats = as.numeric(unlist(df[df$Rk == teamY, seq(9, 15, 2)], use.names = FALSE))
-  y_def_stats = as.numeric(unlist(df[df$Rk == teamY, seq(10, 16, 2)], use.names = FALSE))
+  y_seed = team_info[team_info$Name == teamY, 2][[1]]
+  y_off_stats = c(-1*y_seed)
+  y_def_stats = c(0)
+    
+  for (s in seq(9, 15, 2))
+  {
+    x_off_stats = c(x_off_stats, df[df$Team == teamX, s][[1]])
+    x_def_stats = c(x_def_stats, df[df$Team == teamX, s + 1][[1]])
+    
+    y_off_stats = c(y_off_stats, df[df$Team == teamY, s][[1]])
+    y_def_stats = c(y_def_stats, df[df$Team == teamY, s + 1][[1]])
+  }
+  
+  
+  #print(teamX)
+  #print(x_off_stats)
+  #print(x_def_stats)
+  
+  #print(teamY)
+  #print(y_off_stats)
+  #print(y_def_stats)
   
   prob = Px(x_off_stats, x_def_stats, y_off_stats, y_def_stats)
   
@@ -137,7 +199,7 @@ simulate_tournament <- function()
 }
 
 
-#simulate_tournament()
+simulate_tournament()
 
 
 
